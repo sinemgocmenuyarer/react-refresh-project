7050a707f8a122544a2de0d6f11dd4a67898d935
sinemgocmenuyarer <sinemgocmenuyarer@gmail.com>
2025-12-12 11:46:44 +0100

    feat:update context

--- code/10 Advanced State Management with Context useReducer/01-starting-project/src/store/context.jsx
+++ code/10 Advanced State Management with Context useReducer/01-starting-project/src/store/context.jsx
@@ -3,4 +3,5 @@ import { createContext } from "react";
 // This CardContext will be a react component in the end so we start with capital letter.
 export const CartContext = createContext({
   items: [],
+  addItemToCart: () => {},
 });
