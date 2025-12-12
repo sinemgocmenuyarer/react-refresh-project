af8d16c97f222f42f5329d2137278596cac6158d
sinemgocmenuyarer <sinemgocmenuyarer@gmail.com>
2025-12-12 11:26:51 +0100

    feat:add context to the project insteaad props drilling

---
+++ code/10 Advanced State Management with Context useReducer/01-starting-project/src/store/context.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+// This CardContext will be a react component in the end so we start with capital letter.
+export const CartContext = createContext({
+  items: [],
+});
